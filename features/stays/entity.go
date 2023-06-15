a9ff2f6ca67d2d8549dae5a033e46dc801b523f4
Kurniawan <kurnhyalcantara@gmail.com>
2023-06-15 09:03:14 +0800

    hotfix: remove InsertImage data interface

--- features/stays/entity.go
+++ features/stays/entity.go
@@ -54,7 +54,6 @@ type StayDataInterface interface {
 	SelectAll() (allStays []Core, err error)
 	Update(stayId string, stayData CoreStayRequest) error
 	Delete(stayId string) error
-	InsertImage(stayId string, coreStayImage CoreStayImageRequest)
 }
 
 type StayServiceInterface interface {
